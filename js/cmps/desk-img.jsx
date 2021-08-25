4522121cd81ccd43f4f7a495e3904950d5e74b95
Moshe Dabush <moshedabush@h-MacBook-Air-sl-Moshe.local>
2021-08-25 14:56:35 +0300

    app-home+deskimg+loader+css improved

---
+++ js/cmps/desk-img.jsx
@@ -0,0 +1,3 @@
+export function Desk() {
+    return <section className="desk"><img src="../assets/css/img/desk.svg"/></section>;
+}
--- js/pages/app-home.jsx
+++ js/pages/app-home.jsx
@@ -1,1 +1,18 @@
-{}
+import { Desk } from "../cmps/desk-img.jsx"
+
+const { NavLink } = ReactRouterDOM
+
+
+export function AppHome() {
+    return (
+        <section className="Home">
+           <NavLink to="/"> <h1>AppSus</h1></NavLink>
+            <nav>
+                {/* <NavLink to="/book" >Our Books</NavLink> */}
+                <NavLink activeClassName="my-active" exact to="/" >Home</NavLink>
+                <Desk/>
+            </nav>
+                
+        </section>
+    )
+}
