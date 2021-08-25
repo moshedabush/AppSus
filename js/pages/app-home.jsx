87346facd100046d38c9f5f66ba2ea826ec34304
Moshe Dabush <moshedabush@h-MacBook-Air-sl-Moshe.local>
2021-08-26 01:41:47 +0300

    icons-added+css improved

--- js/pages/app-home.jsx
+++ js/pages/app-home.jsx
@@ -6,12 +6,16 @@ const { NavLink } = ReactRouterDOM
 export function AppHome() {
     return (
         <section className="Home">
-           <NavLink to="/"> <h1>AppSus</h1></NavLink>
+           {/* <NavLink to="/"> <h1>AppSus</h1></NavLink> */}
+           <NavLink to="/"> <img className="home-logo" src="../assets/css/img/horse-icon2.png" /></NavLink>
             <nav>
-                <NavLink activeClassName="my-active" exact to="/" >Home</NavLink>
-                <NavLink to="/book" >Book</NavLink>
-                <NavLink to="/note" >Note</NavLink>
-                <NavLink to="/about" >About</NavLink>
+                {/* <NavLink activeClassName="my-active" exact to="/" >Home</NavLink> */}
+                <div className="apps-icons">
+                <NavLink to="/book" ><img src="../assets/css/img/book-icon.png"/></NavLink>
+                <NavLink to="/note" className="note-icon" ><img src="../assets/css/img/note-icon.png"/></NavLink>
+                <NavLink to="/" ><img src="../assets/css/img/email-icon.png"/></NavLink>
+                </div>
+                <NavLink to="/about" className="about-logo"><img src="../assets/css/img/about-logo.png"/></NavLink>
                 <Desk/>
             </nav>
                 
