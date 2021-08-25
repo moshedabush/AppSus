bf0d39a50fa5b3743bfc011af006c365bacb21e8
koren turgeman <koren3210@gmail.com>
2021-08-25 11:19:03 +0300

    korencommit

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -1,16 +1,9 @@
-
 const Router = ReactRouterDOM.HashRouter
 const { Route, Switch } = ReactRouterDOM
 
-<<<<<<< HEAD
-import { AppHeader } from './js/cmps/AppHeader.jsx';
+import { AppHeader } from './js/cmps/AppHeader';
 // import { BookApp } from './pages/BookApp.jsx';
-=======
-import { AppHeader } from "./js/cmps/AppHeader.jsx"
-import { AppHome } from "./js/pages/app-home.jsx"
->>>>>>> b3a8010ac182e26716ad157f3a597ab31c793634
 // import { About } from './pages/About.jsx';
-// import { BookApp } from './pages/BookApp.jsx';
 // import { Home } from './pages/Home.jsx';
 // import { BookDetails } from './pages/BookDetails.jsx';
 
@@ -19,16 +12,16 @@ export function App() {
 
     <Router>
       <header>
-        <AppHeader />
+        <AppHeader/>
       </header>
       <main>
-        <Switch>
+      <Switch>
 
-          {/* <Route path="/book/:bookId" component={BookDetails}/>
+        {/* <Route path="/book/:bookId" component={BookDetails}/>
         <Route path="/book" component={BookApp}/>
-        <Route path="/about" component={About}/> */}
-          <Route path="/" component={AppHome} />
-        </Switch>
+        <Route path="/about" component={About}/>
+        <Route path="/" component={Home}/> */}
+      </Switch>
       </main>
     </Router>
   );
